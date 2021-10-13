c125e591c2922dff17876db394ef7f941eb40658
Yash Gupta <73610050+YashJA-dev@users.noreply.github.com>
2021-10-14 04:33:08 +0530

    Time: 351 ms (29.38%), Space: 120 MB (13.56%) - LeetHub

--- maximum-number-of-removable-characters/maximum-number-of-removable-characters.java
+++ maximum-number-of-removable-characters/maximum-number-of-removable-characters.java
@@ -1,42 +1,30 @@
 class Solution {
     public int maximumRemovals(String s, String p, int[] removable) {
-        if (s == null || s.length() == 0)
-            return 0;
-        // we meed to binary search for the k which need to be removed
-        char [] convertedS = s.toCharArray();
-        int left = 0;
-        int right = removable.length - 1;
-        while (left <= right) {
-            int middle = (left + right) / 2;
-            //int middle = left + (right - left) / 2;
-            // remove letters from 0 to mid by changing it into someother non letters
-            for (int i=0; i<=middle; i++) {
-                convertedS[removable[i]] = '?';
-            }
-            // if it is still subsequence change left boundary 
-            // else replace all eremoved ones and change right boumndary
-            if (isSubsequence(convertedS, p)) {
-                left = middle + 1;
-            }
-            else {
-                for (int i=0; i<=middle; i++) {
-                    convertedS[removable[i]] = s.charAt(removable[i]);
-                }
-                right = middle - 1;
+       int l=0,r=removable.length;
+        int ans=0;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(krishnJi_k_charan(removable,new StringBuilder(s),new StringBuilder(p),mid)){
+                ans=mid;
+                l=mid+1;
+            }else{
+                r=mid-1;
             }
         }
-        return left;
+        return ans;
     }
     
     // simple check for subsequence
-    public boolean isSubsequence(char [] convertedS, String p) {
-        int p1 = 0;
-        int p2 = 0;
-        while (p1 < convertedS.length && p2 < p.length()) {
-            if (convertedS[p1] != '?' && convertedS[p1] == p.charAt(p2))
-                p2 += 1;
-            p1 += 1;
+    public boolean krishnJi_k_charan(int[] nums,StringBuilder s,StringBuilder t,int k){
+        for(int i=0;i<k;i++){
+            s.setCharAt(nums[i],'?');
         }
-        return p2 == p.length();
+        int j=0;
+        for(int i=0;i<s.length();i++){
+            if(j<t.length()&&s.charAt(i)==t.charAt(j)){
+                j++;
+            }
+        }
+        return j==t.length();
     }
 }
