e12fa6c6f162d17a13b5bfdd9969d2e22a6042a4
Yash Gupta <73610050+YashJA-dev@users.noreply.github.com>
2021-10-15 00:09:27 +0530

    Time: 120 ms (42.93%), Space: 52 MB (59.16%) - LeetHub

--- stone-game-vi/stone-game-vi.java
+++ stone-game-vi/stone-game-vi.java
@@ -1,16 +1,9 @@
 class Solution {
     public int stoneGameVI(int[] aliceValues, int[] bobValues) {
         PriorityQueue<Stone> q = new PriorityQueue<Stone>((a, b) -> b.alice + b.bob - a.alice - a.bob);
-        // sort each stone in decreasing on the basis of it's worth
-        // each player will remove a stone which helps them score more for themselves
-        // as well as prevents the other player from scoring more
-        // so a stone with a larger total value (i.e. value for alice + value for bob) is more worthy
-        // and thus will be removed first
-        
         for(int i = 0; i < aliceValues.length; i++) {
             q.add(new Stone(aliceValues[i], bobValues[i]));
         }
-        
         int ac = 0, bc = 0;
         boolean aliceTurn = true;
         while(!q.isEmpty()) {
@@ -18,7 +11,6 @@ class Solution {
             else bc += q.poll().bob;
             aliceTurn = !aliceTurn;
         }
-        
         return Integer.compare(ac, bc);
     }
     
